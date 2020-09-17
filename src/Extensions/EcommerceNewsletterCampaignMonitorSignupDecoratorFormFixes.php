b3de7db42d20ec6681028e46ce26b4cd019f4744
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-09-17 13:20:51 +1200

    PATCH: LINTER

--- src/Extensions/EcommerceNewsletterCampaignMonitorSignupDecoratorFormFixes.php
+++ src/Extensions/EcommerceNewsletterCampaignMonitorSignupDecoratorFormFixes.php
@@ -15,6 +15,7 @@ use SilverStripe\Security\Security;
 use SilverStripe\View\Requirements;
 use Sunnysideup\CampaignMonitor\Api\CampaignMonitorAPIConnector;
 use Sunnysideup\Ecommerce\Model\Config\EcommerceDBConfig;
+use Sunnysideup\Ecommerce\Config\EcommerceConfig;
 
 class EcommerceNewsletterCampaignMonitorSignupDecoratorFormFixes extends Extension
 {
